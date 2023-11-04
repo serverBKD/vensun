488f03ba725f7cd39ab3abaff485b4ad1184283f
serverBKD <serverbkd@outlook.com>
2023-11-04 14:55:13 -0400

    2023nov04: v0.1.1.7: bugs PreBuilds footer + WS

--- src/components/WhatsappButton.tsx
+++ src/components/WhatsappButton.tsx
@@ -1,15 +1,16 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { WhatsappButtonItems } from '@/types/const.ts'
+// import { WhatsappButtonItems } from '@/types/const.ts'
+// import { LinkItems } from '@/types/types'
 
 export default function WhatsappButton() {
 	return (
 		<div className="fixed w-20 h-20 text-red-900 rounded-full  bottom-6 right-2 ">
 			<div className="w-150 h-150 rounded-full absolute bottom-6 right-2">
-				<Link href={WhatsappButtonItems.linkUrl} rel="noopener noreferrer" target="_blank" >
+				<Link href={'WhatsappButtonItems.linkUrl'} rel="noopener noreferrer" target="_blank" >
 					<Image
-						src={WhatsappButtonItems.linkSrc}
-						alt={WhatsappButtonItems.linkName}
+						src={'WhatsappButtonItems.linkUrl'}
+						alt={'WhatsappButtonItems.linkName'}
 						width={150}
 						height={150}
 						quality={100}
